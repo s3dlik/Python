63d580ad81d29a6b8d261bb46ff919caac2e6a99
Filip Sedlařík <filip.sedlarik@gmail.com>
2022-03-04 22:47:22 +0100

    fixed mistakes

--- Django project/faceitStats/connectionHeader.py
+++ Django project/faceitStats/connectionHeader.py
@@ -3,7 +3,7 @@ import requests
 def retrieveDataByID(id):
     url = "https://open.faceit.com/data/v4/players/" + id + "/stats/csgo"
     headers = {
-        'Authorization': 'Bearer 207ad3f2-90bc-4456-b0e9-d4592cbbb566',
+        'Authorization': 'Your token here',
         'accept': 'application/json'
     }
     res = requests.get(url, headers=headers)
@@ -13,7 +13,7 @@ def retrieveDataByID(id):
 def retrieveDataByNick(nickname):
     url = "https://open.faceit.com/data/v4/players?nickname=" + nickname
     headers = {
-        'Authorization': 'Bearer 207ad3f2-90bc-4456-b0e9-d4592cbbb566',
+        'Authorization': 'Your token here',
         'accept': 'application/json'
     }
     res = requests.get(url, headers=headers)
@@ -24,7 +24,7 @@ def connectionReq(nick):
     url = "https://open.faceit.com/data/v4/players?nickname=" + nick
 
     headers = {
-        'Authorization': 'Bearer 207ad3f2-90bc-4456-b0e9-d4592cbbb566',
+        'Authorization': 'Your token here',
         'accept': 'application/json'
     }
     res = requests.get(url,headers=headers)
--- Django project/project/settings.py
+++ Django project/project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7co-2ep=vbd9&+)#xldzugq27tep#kpy3fcrmr+dr^%j)73qtd'
+SECRET_KEY = key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
